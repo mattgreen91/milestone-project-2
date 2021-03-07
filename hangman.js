4a3f171a20de269a80fa336ea002b07f1051f5a7
mattgreen91 <mattgreen91@outlook.com>
2021-03-07 06:46:14 +0000

    created basic frame for hangman game.  still need to add lives, and check game score

--- hangman.js
+++ hangman.js
@@ -1,51 +1,88 @@
 //hangman game
-function startGame() {
-    let answers = [
-    "mercury",
-    "venus",
-    "earth",
-    "mars",
-    "jupiter",
-    "saturn",
-    "uranus",
-    "neptune",
-    "pluto"
-    ];
-    let answer = "";
-    let lives = 5;
+
+window.onload = resetGame();
+
+var gameAnswerLetters;
+var hiddenAnswerLetters;
+var gameAnswer;
+
+function resetGame() {
+    //new game starts with won and lost as 0-0
     let won = 0;
     let lost = 0;
-    let guessed = [];
-    let i = Math.floor(Math.random() * answers.length);
-    answer = answers[i];
-};
+    //reset game passes reset won and lost values into buildGame function
+    buildGame(won, lost);
+}
 
-startGame();
+//buildGame recieves the won and lost values from resetGame, or checkWon fuction
+function buildGame(score1, score2) {
+    buildKeyboard();
+    won = score1;
+    lost = score2;
 
-function createKeyboard() {
+    console.log(won);
+    console.log(lost);
+
+    document.getElementById('won').innerHTML = won;
+    document.getElementById('lost').innerHTML = lost;
+
+
+    let allAnswers = [
+        "mercury",
+        "venus",
+        "earth",
+        "mars",
+        "jupiter",
+        "saturn",
+        "uranus",
+        "neptune",
+        "pluto"
+    ];
+
+    //selects one answer randomly
+    let i = Math.floor(Math.random() * allAnswers.length);
+    gameAnswer = allAnswers[i];
+    //converts the word into an array of letters
+    gameAnswerLetters = gameAnswer.split('');
+    //creates an empty array of hidden answer letters
+    hiddenAnswerLetters = [];
+    //loop will replace the letter with a "-" for the length of the gameAnswer
+    for (i = 0; i < gameAnswer.length; i++)
+        hiddenAnswerLetters.push("-");
+
+    //writes the hidden word on the screen
+    document.getElementById('word').innerHTML = hiddenAnswerLetters.join();
+
+}
+
+//function creates a keyboard layout
+function buildKeyboard() {
+    //spits letters into individal arrays
     let keys = "abcedefghijklmnopqrstuvwxyz".split("").map(letter =>
+        //arrays are assigned individual buttons
         `
         <button id='` + letter + `' onClick="guessAnswer('` + letter + `')">
         ` + letter + `
-      </button>
-    `).join(''); //removes the commas separting buttons
-
-  document.getElementById('letters').innerHTML = keys;
-};
-
-createKeyboard();
-
-function guessAnswer(key) {
-    guessed.indexOf(key) === -1 ? guessed.push(key) : null;
-    document.getElementById(key).setAttribute('disabled', true);
-    if (answer.indexOf(key) >=0) {
-        makeGuess();
-    } else {
-lifeLost();
-    }
+        </button>
+        `
+        //join removes the commas separting buttons
+    ).join('');
+    //the letters are written onto the button
+    document.getElementById('letters').innerHTML = keys;
 }
 
-function lifeLost() {
-    lives = lives-1;
-    document.getElementById("lives").innerHTML = lives;
-};
+//when a key is pressed, this function will check it matches the word in the game
+function guessAnswer(key) {
+    let guess = [];
+    guess.indexOf(key) === -1 ? guess.push(key) : null;
+    //this disables the button once it has been pressed
+    document.getElementById(key).setAttribute('disabled', true);
+    let i = 0;
+    //this loop searches if the key pressed matches any letters in the the game answer
+    while (i < gameAnswer.length) {
+        if (key === gameAnswerLetters[i]) {
+            hiddenAnswerLetters[i] = gameAnswerLetters[i];
+            document.getElementById('word').innerHTML = hiddenAnswerLetters.join();
+        } i++;
+    }
+}
