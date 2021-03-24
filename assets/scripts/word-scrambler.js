4b83e30013a4987e57c3f966084b21e1f1d6100c
mattgreen91 <mattgreen91@outlook.com>
2021-03-24 14:25:35 +0000

    created a basic game with Astronaught word using JQuery.  Made a blank JSON folder, and will begin to create directory of words

---
+++ assets/scripts/word-scrambler.js
@@ -0,0 +1,19 @@
+// reset text field to blank  
+$('input[type="text"]').val("");
+// next game
+$(".scrambled-word").text("THOGASATURN");
+// when submit button is pressed
+$("#word-scrambler-game").on("submit", function(event) {
+  event.preventDefault();
+  let $submit = $(this).find("[type=text]");
+  let answer = $submit.val().toUpperCase();
+  // checks if the answer typed matches the word
+  if (answer === "ASTRONAUGHT" || answer === "astronaught") {
+    // displays the result
+    alert("Well done! You got it right :)");
+    $('input[type="text"]').val("");
+    $(".scrambled-word").text("TOCERK");
+  } else {
+    alert("Sorry, that was the wrong answer :(\nPlease try again")
+  }
+});
