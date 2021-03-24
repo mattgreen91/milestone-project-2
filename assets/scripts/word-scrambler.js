88c6ec2dfa2d120453b6e912c1e28ca830406bba
mattgreen91 <mattgreen91@outlook.com>
2021-03-24 16:39:11 +0000

    managed to import JSON file, need to work on trying to get jumbled word

--- assets/scripts/word-scrambler.js
+++ assets/scripts/word-scrambler.js
@@ -5,21 +5,40 @@ $.getJSON("./assets/json/word-scrambler.json", function(gameData) {
     let moons = gameData.moons;
     let objects = gameData.objects;
 });
+
+let category = objects;
+
+$("#change-objects").on("click", function(event) {
+    category = objects;
+});
+$("#change-galaxies").on("click", function(event) {
+category = galaxies;
+});
+$("#change-moons").on("click", function(event) {
+category = moons;
+});
+$("#change-planets").on("click", function(event) {
+category = planets;
+});
+
+let rand = Math.floor(Math.random() * category.length);
+let word = category[rand];
+
 // reset text field to blank  
 $('input[type="text"]').val("");
 // next game
-$(".scrambled-word").text("THOGASATURN");
+$(".scrambled-word").text(word);
 // when submit button is pressed
 $("#word-scrambler-game").on("submit", function(event) {
   event.preventDefault();
   let $submit = $(this).find("[type=text]");
   let answer = $submit.val().toUpperCase();
   // checks if the answer typed matches the word
-  if (answer === "ASTRONAUGHT" || answer === "astronaught") {
+  if (answer === word) {
     // displays the result
     alert("Well done! You got it right :)");
     $('input[type="text"]').val("");
-    $(".scrambled-word").text("TOCERK");
+    $(".scrambled-word").text(category[rand]);
   } else {
     alert("Sorry, that was the wrong answer :(\nPlease try again")
   }
