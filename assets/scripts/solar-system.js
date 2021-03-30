df4a3d5bc9f002b3216f137941c31c79b248a86e
mattgreen91 <mattgreen91@outlook.com>
2021-03-30 14:54:56 +0100

    started adding onclick functions to the planets

---
+++ assets/scripts/solar-system.js
@@ -0,0 +1,32 @@
+function sun() {
+}
+
+function mercury() {
+}
+
+function venus() {
+
+} 
+
+function earth() {
+
+}
+
+function mars() {
+
+}
+function jupiter() {
+
+}
+function saturn() {
+
+}
+function uranus() {
+
+}
+function neptune() {
+
+}
+function pluto () {
+    
+}
